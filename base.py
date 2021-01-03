6221158fed0a9ea8cadea9230f06072734887842
jumbobot <kk.banerimhanov@gmail.com>
2021-01-03 22:10:54 +0300

    1.0
    Signed-off-by: jumbobot <kk.banerimhanov@gmail.com>

---
+++ base.py
@@ -0,0 +1,32 @@
+import requests
+import json
+
+# Парсинг с вк группы с помощью vk_api
+def main():
+    #https://api.vk.com/method/wall.get?user_id=210700286&v=5.52
+    group_id="-54530371" #id группы которую мы парсим
+    access_token = "df439f51df439f51df439f51efdf3628e1ddf43df439f5180affe39047548ba50381df8"#Сервисный ключ доступа
+    version = 5.52
+    offset = 0
+    count = int(input()) #кол-во "парсируемых" постов
+    r = requests.get('https://api.vk.com/method/wall.get',params={'owner_id': group_id,
+                                                                  'count': count,
+                                                                  'offset': offset,
+                                                                  'access_token':access_token,
+                                                                 'v': version})
+
+
+    # Выводим инф-ю в удобном формате
+    json_output = json.loads(r.text)['response']['items']
+
+
+    # Среди всех постов выделяем и выводим только те что имеют тэг '#book@proglib', или же просто выводим только книги
+    for posts in json_output:
+        for elem in posts:
+            if elem == 'text' and '#book@proglib' in posts[elem]:
+                print(posts[elem],end='\n\n\n\n\n\n')
+
+
+
+if __name__ == '__main__':
+    main()
